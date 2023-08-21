a92a4693afaf907c7b445f0db4dee774913768ab
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-08-21 22:32:06 +0300

    init:commit add edit

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::get('/', function () {
 
 Route::get('/{page}' , [AdminController::class , 'index'])->name('index');
 
-Aux th::routes();
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
