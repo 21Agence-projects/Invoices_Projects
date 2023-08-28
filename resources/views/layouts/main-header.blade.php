76c2c596f01a1db4ee25988d00429ee7b3bbcb14
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-08-28 14:11:09 +0300

    init:commit edit main-sidebar

--- resources/views/layouts/main-header.blade.php
+++ resources/views/layouts/main-header.blade.php
@@ -308,7 +308,7 @@
                                 <div class="main-img-user"><img alt=""
                                         src="{{ URL::asset('assets/img/faces/6.jpg') }}" class=""></div>
                                 <div class="mr-3 my-auto">
-                                    <h6>Petey Cruiser</h6><span>Premium Member</span>
+                                    <h6>{{ Auth::user()->name }}</h6><span>{{ Auth::user()->email }}</span>
                                 </div>
                             </div>
                         </div>
@@ -319,7 +319,7 @@
                         <a class="dropdown-item" href=""><i class="bx bx-slider-alt"></i> Account Settings</a>
                         <a class="dropdown-item" href="{{ route('logout') }}"
                             onclick="event.preventDefault();document.getElementById('logout-form').submit();"><i
-                                class="bx bx-log-out"></i>تسجيل خروج
+                                class="bx bx-log-out"></i> Logout
                         </a>
                         <form id="logout-form" action="{{ route('logout') }}" method="POST"
                             style="display: none;">
