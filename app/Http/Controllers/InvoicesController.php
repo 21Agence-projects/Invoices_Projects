4960c1f5658a61b0529f62b3d2b6d7f9c4c21571
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-09-12 10:09:44 +0300

    init:commit add invoice store controller

--- app/Http/Controllers/InvoicesController.php
+++ app/Http/Controllers/InvoicesController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\invoices;
 use App\Models\sections;
+use App\Models\invoice_attachments;
 use Illuminate\Http\Request;
+use App\Models\invoices_details;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class InvoicesController extends Controller
 {
@@ -54,7 +58,47 @@ class InvoicesController extends Controller
             'note' => $request->note,
         ]);
 
-        $invoice_id  = invoices::latest()->first()->id;
+        $invoice_id = invoices::latest()->first()->id;
+        invoices_details::create([
+            'id_Invoice' => $invoice_id,
+            'invoice_number' => $request->invoice_number,
+            'product' => $request->product,
+            'Section' => $request->Section,
+            'Status' => 'غير مدفوعة',
+            'Value_Status' => 2,
+            'note' => $request->note,
+            'user' => (Auth::user()->name),
+        ]);
+
+        if ($request->hasFile('pic')) {
+            $this->validate(
+                $request,
+                ['pic' => 'required|max:10000'],
+                ['pic.mines' => 'تم حفظ'],
+            );
+
+            $invoice_id = invoices::latest()->first()->id;
+            $image = $request->file('pic');
+            $file_name = $image->getClientOriginalName();
+            $invoice_number = $request->invoice_number;
+
+            $attachments = new invoice_attachments();
+
+            $attachments->file_name = $file_name;
+            $attachments->invoice_number = $invoice_number;
+            $attachments->Created_by = Auth::user()->name;
+            $attachments->invoice_id = $invoice_id;
+
+            $attachments->save();
+
+            //mov pic
+
+            $imageName = $request->pic->getClientOriginalName();
+            $request->pic->move(public_path('Attachments/' . $invoice_number) , $imageName);
+        }
+
+        session()->flash('Add' , 'success Add');
+        return back();
     }
 
     /**
